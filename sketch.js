78e1a74467312a537a19a3fb41b26fb2ec716aa9
Dylan SIgler <dylan.sigler@bylight.com>
2020-07-16 15:22:28 -0500

    Added text and started styling. Also got html overlayed on top of p5 canvas

--- sketch.js
+++ sketch.js
@@ -1,54 +1,88 @@
-let WIDTH = 1000;
-let HEIGHT = 700;
-
 let numPlanets;
 let numStars;
 
 let planets = [];
 let stars = [];
 
-let words = 'Dylan Sigler',
+let name = 'Dylan Sigler',
   font,
-  bounds,
-  fontsize = 32,
-  x,
-  y;
+  name_fontsize = 32,
+  sub_fontsize = 20,
+  name_x,
+  name_y;
+
+let email = 'jsigler47@gmail.com',
+  e_x,
+  e_y;
+
+let github = 'github',
+  g_x,
+  g_y;
+
+let linkedin = 'linkedin',
+  l_x,
+  l_y;
+
+function draw_text(){
+  let R = 255,
+      G = 255,
+      B = 255;
+  //R = map(mouseX, 0, windowWidth, 50, 255);
+  //G = map(mouseY, 0, windowHeight, 50, 255);
+  //B = map(mouseX + mouseY, 0, windowWidth + windowHeight, 50, 255);
+  fill(R, G, B)
+  textAlign(CENTER);
+  textSize(name_fontsize);
+  text(name, 200, windowHeight - 100);
+
+  textAlign(LEFT);
+  textSize(sub_fontsize);
+  text(email, windowWidth - 300, 100)
+  text(linkedin, windowWidth - 300, 125);
+  text(github, windowWidth - 300, 150)
+
+}
 
 function preload(){
   font = loadFont('assets/AstroSpace-0Wl3o.otf')
 }
 
+function windowResized(){
+	resizeCanvas(windowWidth, windowHeight);
+	for (i = 0; i < numStars; i++) {
+      stars[i].x = random(windowWidth);
+      stars[i].y = random(windowHeight);
+ 	}
+}
+
 function setup() {
   numPlanets = random(3, 10);
   numStars = random(400, 1000);
-  createCanvas(WIDTH, HEIGHT);
-  textAlign(CENTER, CENTER);
+
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0,0);
+  canvas.style('z-index', '-1')
+
   ellipseMode(CENTER)
   angleMode(DEGREES)
-  
   textFont(font);
-  textSize(fontsize);
-  bounds = font.textBounds(words, 0, 0, fontsize);
-  x = width / 2 - bounds.w / 2;
-  y = height / 2 - bounds.h / 2;
 
   for (i = 0; i < numPlanets; i++) {
     planets[i] = new Planet(
       random(10, 35), //radius
-      random(40, HEIGHT / 2), //distance
+      random(40, windowHeight / 2), //distance
       random(-1.6, 1.6)); //speed
   }
   for (i = 0; i < numStars; i++) {
-    stars[i] = new Star(random(WIDTH), random(HEIGHT), 1)
+    stars[i] = new Star(random(windowWidth), random(windowHeight), 1)
   }
 }
 
 function draw() {
-  let time = millis();
   background(0);
   noStroke();
   fill(245, 255, 66);
-  ellipse(WIDTH / 2, HEIGHT / 2, 40, 40);
+  ellipse(windowWidth / 2, windowHeight / 2, 40, 40);
 
   for (i = 0; i < numStars; i++) {
     stars[i].star();
@@ -57,18 +91,7 @@ function draw() {
     planets[i].planet();
   }
   
-  text(words, 200, 500);
-  bounds = font.textBounds(words, x, y, fontsize);
-  
-   if (
-     mouseX >= bounds.x &&
-     mouseX <= bounds.x + bounds.w &&
-     mouseY >= bounds.y &&
-     mouseY <= bounds.y + bounds.h
-   ) {
-     x += random(-5, 5);
-     y += random(-5, 5);
-   }
+  draw_text()
 }
 
 class Planet {
@@ -78,13 +101,12 @@ class Planet {
     this.speed = speed;
     this.color = color(random(0, 255), random(0, 255), random(0, 255));
     this.angle1 = random(0, 500);
-    this.angle2 = 0;
   }
 
   planet() {
     resetMatrix();
     push();
-    translate(WIDTH / 2, HEIGHT / 2);
+    translate(windowWidth / 2, windowHeight / 2);
     rotate(this.angle1);
     this.angle1 = this.angle1 + this.speed
     fill(this.color);
